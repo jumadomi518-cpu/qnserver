19dd875e88a3dfcd9bde6b2739cd1682578d3dbd
Dominic juma <jumadomi518@gmail.com>
2026-05-10 18:51:06 +0300

    c

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173/"}));
+app.use(cors({ origin: "https://qnfrontend.vercel.app/"}));
 const ai = new GoogleGenAI({ apiKey: process.env.API });
 
 
